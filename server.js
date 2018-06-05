1b78f80f6fd2702a9af31cdae8dc8eb822cdacaa
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-06-05 20:49:39 +1200

    we are awesome

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const hbs = require('express-handlebars')
 const art = require('./art.json')
 
 const server = express()
-module.exports = server
-
-// Middleware
-server.engine('hbs', hbs({
-  extname: 'hbs'
-}))
-server.set('view engine', 'hbs')
-server.use(express.static('public'))
 
 // Routes
 server.get('/', (req, res) => {
@@ -23,3 +15,12 @@ server.get('/', (req, res) => {
   }
   res.render('home', viewData)
 })
+
+server.engine('hbs', hbs({
+  extname: 'hbs'
+  // defaultLayout: 'main'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+
+module.exports = server
