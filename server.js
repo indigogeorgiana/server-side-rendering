318ad2ebedf49839b8a70207eb9ba7b635b6e4c2
ZeBin Xiao <juniorxiao01@gmail.com>
2018-06-05 13:42:25 +1200

    finished step 6

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const art = require('./art.json')
 
 const server = express()
 module.exports = server
@@ -12,3 +13,19 @@ server.set('view engine', 'hbs')
 server.use(express.static('public'))
 
 // Routes
+server.get('/', (req, res) => {
+  const viewData = {
+    title: 'Gallery',
+    art: art
+  }
+  res.render('home', viewData)
+})
+
+server.get('/artworks/:id', (req, res) => {
+  const artId = req.params.id
+  function isId (idNumber) {
+    return idNumber.id === Number(artId)
+  }
+  const artW = art.find(isId)
+  res.render('./partials/artworks', artW)
+})
